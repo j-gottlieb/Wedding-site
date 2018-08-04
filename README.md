e3a69a6dfaefa5d3c25b7bd0467fe49834fa42d2
JESSE GOTTLIEB <jessegottlieb@JESSEs-MacBook-Pro.local>
2018-08-04 07:28:38 -0400

    lets try this again

---
+++ README.md
@@ -0,0 +1,1 @@
+# j-gottlieb.github.io
---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+var countDownDate = new Date('Sep 5, 2020 18:00:00').getTime();
+
+var x = setInterval (function() {
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	document.getElementById('countdown').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById('countdown').innerHTML = "It's today!";
+	}
+}, 1000);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById('top').style.display = 'block';
+	} else {
+		document.getElementById('top').style.display = 'none';
+	}
+};
+
+function topFunction () {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
